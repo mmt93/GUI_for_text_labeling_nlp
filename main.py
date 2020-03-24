f15d253c288898b8e6b5a1d99712455ade3a8c57
mmt93 <41652032+mmt93@users.noreply.github.com>
2020-03-24 14:11:21 -0300

    Update main.py
    A better UI/UX.

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ def text_displayed(value):
     result_counter = 0
     text_len = df['text'].last_valid_index()
     cat_len = df['category'].last_valid_index()
-    
+    progress_text.set('{}/{}'.format(cat_len,text_len))
+
     if cat_len is None:
         cat_len = 0
 
@@ -52,34 +53,41 @@ def open_file():
 
 #Windows Settings
 root = tk.Tk()
-root.geometry("800x800")
-root.grid_propagate(False)
+root.geometry("850x800")
+root.configure(background='white')
+#root.grid_propagate(False)
 
 #Text Settings
 text = tk.StringVar()
 open_file()
-a = tk.Label(root, textvariable=text, wraplength = 800, font=(None,12))
-a.pack()
+a = tk.Label(root, textvariable=text, wraplength = 700, font=("Verdana",12), width= 70)
+a.grid(column=2, row=3, sticky=tk.S, columnspan=6)
+a.configure(background='white')
 
 #Declaring buttons of each category
 open_csv = tk.Button(root, text="Open CSV...", command=lambda: open_file())
-open_csv.place(relx=0.0, rely=0.0, anchor=tk.NW)
+open_csv.grid(column=1, row=1, sticky=tk.NW)
 cat_delivery = tk.Button(root, text="Entregas/Prazos", command=lambda: text_displayed(0))
-cat_delivery.pack(side=tk.LEFT,expand=tk.TRUE)
+cat_delivery.grid(column=1, row=2, sticky=tk.S, pady = 2)
 cat_cashback = tk.Button(root, text="Cashback", command=lambda: text_displayed(1))
-cat_cashback.pack(side=tk.LEFT,expand=tk.TRUE)
+cat_cashback.grid(column=2, row=2, sticky=tk.S, pady = 2)
 cat_il_payment = tk.Button(root, text="Cobrança/Pagamento", command=lambda: text_displayed(2))
-cat_il_payment.pack(side=tk.LEFT,expand=tk.TRUE)
+cat_il_payment.grid(column=3, row=2, sticky=tk.S, pady = 2)
 cat_warranty = tk.Button(root, text="Defeito/Garantia", command=lambda: text_displayed(3))
-cat_warranty.pack(side=tk.LEFT, expand=tk.TRUE)
+cat_warranty.grid(column=4, row=2, sticky=tk.S, pady = 2)
 cat_cancel = tk.Button(root, text="Cancelamento", command=lambda: text_displayed(4))
-cat_cancel.pack(side=tk.LEFT,expand=tk.TRUE)
+cat_cancel.grid(column=5, row=2, sticky=tk.S, pady = 2)
 cat_calling = tk.Button(root, text="Ligação", command=lambda: text_displayed(5))
-cat_calling.pack(side=tk.LEFT, expand=tk.TRUE)
+cat_calling.grid(column=6, row=2, sticky=tk.S, pady = 2)
 cat_others = tk.Button(root, text="Outros", command=lambda: text_displayed(6))
-cat_others.pack(side=tk.LEFT, expand=tk.TRUE)
+cat_others.grid(column=7, row=2, sticky=tk.S, pady = 2)
 save = tk.Button(root, text="save", command=lambda: save_to_file(), bg='green', font=(None,14))
-save.place(relx=1.0, rely=1.0, anchor=tk.SE)
+save.grid(column=8, row=1, sticky=tk.SE)
 
+#Progress info
+progress_text = tk.StringVar()
+progress = tk.Label(root, textvariable=progress_text, wraplength = 800, font=(None,14))
+progress.grid(column=2, row=4, sticky=tk.S)
+progress.configure(background='white')
 #Play
 root.mainloop()
